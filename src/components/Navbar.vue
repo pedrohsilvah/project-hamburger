39927478b1bb6d764bbf68048fa6033b496465b7
pedro <pedrohsilvasant@gmail.com>
2024-03-15 13:00:35 -0300

    organizando rotas e navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav>
+        <router-link to="/">
+            <img :src="logo" :alt="alt" id="logo">
+        </router-link>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/pedidos">Pedidos</router-link>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "Navbar"
+}
+
+</script>
+
+<style scoped>
+
+</style>
