1fd3289a5d1cf342d979695dad2e1f41cd867186
pedro <pedrohsilvasant@gmail.com>
2024-03-18 13:11:08 -0300

    estilizando navbar e footer

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ import Footer from './components/Footer.vue'
     },
     data() {
       return {
-        logo_src: "../assets/logo.png",
+        logo_src: "/img/logo.png",
         app_name: "Make Your Burger"
       }
     }
@@ -23,5 +23,22 @@ import Footer from './components/Footer.vue'
 </script>
 
 <style>
+  * {
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
 
+  .main-container {
+    margin: 50px;
+    min-height: 250px;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
 </style>
