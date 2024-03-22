d9232ddcfa0584659ed76cbd86747022dadb64cc
pedro <pedrohsilvasant@gmail.com>
2024-03-22 19:20:39 -0300

    estilizando e criando components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <template>
+  <Banner />
   <div class="main-container">
-    <h1>Home</h1>
+    <h1>Monte o seu burger:</h1>
+    <BurgerForm />
   </div>
 </template>
 
 <script>
+  import Banner from "../components/Banner.vue"
+  import BurgerForm from "../components/BurgerForm.vue"
 
+  export default {
+    name: 'HomeView',
+    components: {
+      Banner,
+      BurgerForm
+    }
+  }
 </script>
