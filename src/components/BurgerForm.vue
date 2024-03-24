749ae93c78227b6fa3bd05a422f4c3d51149bf44
pedro <pedrohsilvasant@gmail.com>
2024-03-24 11:28:24 -0300

    manipulando dados

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -2,7 +2,7 @@
     <div>
         <p>componente de mensagem</p>
         <div>
-            <form id="burger-form">
+            <form id="burger-form" @submit="createBurger">
 
                 <div class="input-container">
                     <label for="nome">Nome do cliente</label>
@@ -54,7 +54,6 @@ export default {
             pao: null,
             carne: null,
             opcionais: [],
-            status: "Solicitado",
             msg: null
         }
     },
@@ -67,6 +66,33 @@ export default {
             this.paes = data.paes
             this.carnes = data.carnes
             this.opcionaisdata = data.opcionais
+        },
+        async createBurger(e) {
+            e.preventDefault(e)
+
+            const data = {
+                nome: this.nome,
+                carne: this.carne,
+                pao: this.pao,
+                opcionais: Array.from(this.opcionais),
+                status: "solicitado" 
+            }
+
+            const dataJson = JSON.stringify(data)
+
+            const req = await fetch("http://localhost:3000/burgers", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: dataJson
+            })
+
+            const res = await req.json()
+
+            // limpar os campos
+            this.nome = "";
+            this.carne = "";
+            this.pao = "";
+            this.opcionais = "";
         }
     },
     mounted() {
